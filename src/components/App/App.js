5b0858fee3c3753a2869c918338bd36406ba840a
Ricardo Ramirez <ricardo.ramirez@pnd.as>
2016-11-06 22:15:11 -0600

    Fix responsive issuess

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -28,12 +28,12 @@ class App extends Component {
           .then(function(results) {
             if (results && results.data) {
               postsArr = postsArr.concat(results.data.data);
-              if (results.data.paging && results.data.paging.next) {
-                recursiveAPICall(results.data.paging.next);
-              } else {
-                resolve(postsArr);
-              }
-              // resolve(postsArr);
+              // if (results.data.paging && results.data.paging.next) {
+              //   recursiveAPICall(results.data.paging.next);
+              // } else {
+              //   resolve(postsArr);
+              // }
+              resolve(postsArr);
             } else {
               reject();
             }
@@ -65,25 +65,28 @@ class App extends Component {
     return (
       <div>
         <div className={this.state.isLoading ? "loader loader-default is-active" : ""} data-text="Loading data. Wait a minute."></div>
-        <header className="cf relative fixed-ns w-100 left-0-ns bottom-0-ns right-0-ns bg-white tc tl-l pa3 z-1">
+        <header className="cf relative fixed-ns w-100 left-0-ns bottom-0-ns right-0-ns bg-white tc tl-l pa3 z-2">
           <div className="dib pv1">
             <h1 className="f4 mv0 ttu orange">Social life in dots</h1>
             <p className="mv2 gray">Know how many times you have posted something on Facebook.</p>
             <p className="mv0 dib">Developed by <a href="http://ricardoerl.com" className="link pv1 pv0-l orange mr0 mr3-l" target="_blank">Ricardo Ramírez</a><a href="#" className="link pv1 pv0-l orange db dib-l" target="_blank">About the project &rarr;</a></p>
           </div>
           <div className="fn fr-l tc tr-l pv2">
-            <FacebookLogin
-              appId="1611672629135041"
-              size="small"
-              scope="user_posts"
-              fields="name,email,picture.type(large)"
-              autoLoad={false}
-              callback={this.handleFacebookLogIn} />
+            {this.state.posts.length === 0 &&
+              <FacebookLogin
+                appId="1611672629135041"
+                size="small"
+                scope="user_posts"
+                fields="name,email,picture.type(large)"
+                autoLoad={true}
+                callback={this.handleFacebookLogIn} />
+            }
               <p className="f6 fw3 mv2 light-silver">We do not store or publish anything on your profile.</p>
+              <p className="f6 fw3 mv2"><a href="https://www.facebook.com/settings?tab=applications" className="light-silver underline" target="_blank">Revoke access here (search: Social Life in Dots)</a></p>
           </div>
         </header>
         <div>
-          <div className="pa3 pb6 tc">
+          <div className="pa3 pb3 pb6-ns tc">
             <User name={this.state.name} avatar={this.state.avatar} count={this.state.posts.length} />
             <Timeline posts={this.state.posts} />
           </div>
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 class User extends Component {
   render() {
     return (
-      <div className="bg-white-90 relative fixed-l left-0 right-0 top-2-ns mw5 center pv1 ph3 mb2 tc z-1">
-        <img src={this.props.avatar} className="br-100" width="100" height="100" alt="{this.props.name}"/>
+      <div className="bg-white-90 relative fixed-ns left-0 right-0 top-2-ns mw-100 mw6-m mw5-l center pv1 ph2 mb2 tc z-1 br2">
+        <img src={this.props.avatar} className="br-100" width="100" height="100" alt={this.props.name}/>
         <h1 className="f4 mv2">{this.props.name}</h1>
         {this.props.count > 0 ? (
-          <div>
+          <div className="f6 f5-ns">
             <p>You have made {this.props.count} publications since you created your Facebook account.</p>
-            <p className>Each point represents every time you posted something on Facebook. (Click on any one!)</p>
-            <p className>Scroll and have fun!</p>
+            <p>Each point represents every time you posted something on Facebook. (Click on any one!)</p>
+            <p>Scroll and have fun!</p>
           </div>
         ) : (
           <div>
