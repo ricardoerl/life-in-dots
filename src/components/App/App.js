f943b65deb409419f659a45c9e1c65aa9b81f46e
Ricardo Ramirez <ricardo.ramirez@pnd.as>
2016-11-10 11:25:52 -0600

    Change grammar

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -71,7 +71,7 @@ class App extends Component {
         <header className="cf relative fixed-ns w-100 left-0-ns bottom-0-ns right-0-ns bg-white tc tl-l pa3 z-2">
           <div className="dib pv1">
             <h1 className="f4 mv0 ttu orange">Life in Dots</h1>
-            <p className="mv2 gray">Know how many times you have posted something on Facebook.</p>
+            <p className="mv2 gray">Know how many times you've posted something on Facebook.</p>
             <p className="mv0 dib">Developed by <a href="http://ricardoerl.com" className="link pv1 pv0-l orange mr0 mr3-l" target="_blank">Ricardo Ramírez</a><a href="https://medium.com/@ricardoerl/life-in-dots-107c3e59ffbe#.x0qla08ue" className="link pv1 pv0-l orange db dib-l" target="_blank">About the project &rarr;</a></p>
           </div>
           <div className="fn fr-l tc tr-l pv2">
@@ -84,7 +84,7 @@ class App extends Component {
                 autoLoad={true}
                 callback={this.handleFacebookLogIn} />
             }
-              <p className="f6 fw3 mv2 light-silver">We do not store or publish anything on your profile.</p>
+              <p className="f6 fw3 mv2 light-silver">We don't store or publish anything on your profile.</p>
               <p className="f6 fw3 mv2"><a href="https://www.facebook.com/settings?tab=applications" className="light-silver underline" target="_blank">Revoke access here (search: Life in Dots)</a></p>
           </div>
         </header>
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -8,19 +8,19 @@ class User extends Component {
         <h1 className="f4 mv2">{this.props.name}</h1>
         {this.props.count > 0 ? (
           <div className="f6 f5-ns">
-            <p>You have made {this.props.count} publications since you created your Facebook account.</p>
+            <p>You've made {this.props.count} publications since you created your Facebook account.</p>
             <p>Each point represents every time you posted something on Facebook. (Click on any one!)</p>
             <p className="f6 tl">
               <svg viewBox="0 0 120 120" className="pa1 dib v-mid" width="16" height="16" version="1.1" xmlns="http://www.w3.org/2000/svg">
                 <circle fill="#FFB700" cx="60" cy="60" r="50"/>
               </svg>
-              Status updates.
+              &rarr; Status updates.
             </p>
             <p className="f6 tl">
               <svg viewBox="0 0 120 120" className="pa1 dib v-mid" width="16" height="16" version="1.1" xmlns="http://www.w3.org/2000/svg">
                 <circle fill="#FF6300" cx="60" cy="60" r="50"/>
               </svg>
-              Links, videos, etc.
+              &rarr; Links, videos, etc.
             </p>
             <p>Scroll and have fun!</p>
           </div>
